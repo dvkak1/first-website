0917d79c434fba2f805f7242d78558e20ac9c77f
dvkak1 <dv_kakilala@yahoo.com>
2025-01-14 13:36:44 +0800

    scroll-to-top button added

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,15 @@
 // Hamburger menu selections
 
 // scroll-to-top selection
+const scrollUp = document.querySelector("#scroll-up");
+ 
+scrollUp.addEventListener("click", () => {
+   window.scrollTo({
+    top: 0, 
+    left: 0,
+    behavior: "smooth",
+   });
+});
 
 // Hamburger menu functionality
 
